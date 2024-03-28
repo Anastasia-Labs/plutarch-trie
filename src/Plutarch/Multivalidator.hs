db2cf5e455267764906395d55af57fa18c766cf0
hade <liveithade@gmail.com>
2024-03-28 10:34:06 +0700

    fix: compile errors

--- src/Plutarch/Multivalidator.hs
+++ src/Plutarch/Multivalidator.hs
@@ -2,7 +2,7 @@ module Plutarch.Multivalidator (multivalidator, spend, main) where
 
 import Plutarch.Api.V1 (PCredential (..))
 import Plutarch.Api.V1.AssocMap qualified as AssocMap
-import Plutarch.Api.V2 (PScriptContext, PScriptHash (..), PScriptPurpose (..), PValidator)
+import Plutarch.Api.V2 (PScriptContext, PScriptHash (..), PScriptPurpose (..), PStakeValidator, PStakingCredential (..), PValidator)
 import Plutarch.Builtin (pasConstr)
 import Plutarch.Prelude
 import Plutarch.Unsafe (punsafeCoerce)
@@ -31,9 +31,9 @@ multivalidator :: Term s (PData :--> PScriptContext :--> POpaque) -> Term s (PDa
 multivalidator mintingPolicy spendingValidator = plam $ \redeemerOrDatum scriptContextOrRedeemer -> P.do
     let constrIndex = pfstBuiltin #$ pasConstr # scriptContextOrRedeemer
     pif
-        (0 #== constrIndex)
-        (punsafeCoerce $ mintingPolicy # redeemerOrDatum # (punsafeCoerce scriptContextOrRedeemer))
+        (0 #< constrIndex)
         (spendingValidator # redeemerOrDatum # scriptContextOrRedeemer)
+        (punsafeCoerce $ mintingPolicy # redeemerOrDatum # (punsafeCoerce scriptContextOrRedeemer))
 
 spend :: Term s PValidator
 spend = phoistAcyclic $
@@ -44,28 +44,29 @@ spend = phoistAcyclic $
         txInfoF <- pletFieldsC @'["inputs", "wdrl"] ctxF.txInfo
         ownInput <- pletC $ ptryOwnInput # txInfoF.inputs # ownRef
         ownInputF <- pletFieldsC @'["address"] ownInput
-        PScriptCredential ownInputScriptCred <- pmatchC (pfield @"credential" # ownInputF.address)
+        let ownScriptCred = (pcon . PStakingHash) $ pdcons @"_0" # (pfield @"credential" # ownInputF.address) # pdnil
         return $
-            pmatch (AssocMap.plookup # ownInputScriptCred # txInfoF.wdrl) $ \case
+            pmatch (AssocMap.plookup # ownScriptCred # txInfoF.wdrl) $ \case
                 PJust _ -> (popaque $ pconstant ())
                 PNothing -> perror
 
-main :: Term s PValidator
+main :: Term s PStakeValidator
 main = phoistAcyclic $
-    plam $ \_ redeemer ctx -> unTermCont $ do
+    plam $ \redeemer ctx -> unTermCont $ do
         ctxF <- pletFieldsC @'["txInfo", "purpose"] ctx
         txInfoF <- pletFieldsC @'["wdrl"] ctxF.txInfo
         return $
             pmatch ctxF.purpose $ \case
-                PMinting policy' ->
-                    let ownPolicyId = pfield @"_0" # policy'
-                     in pmatch (AssocMap.plookup # (PScriptCredential pdcons # pdata (pcon $ PScriptHash $ phexByteStr ownPolicyId) # pdnil) # txInfoF.wdrl) $ \case
+                PMinting ((pfield @"_0" #) -> policy) -> do
+                    let csByteString = (pfromData . pto) policy
+                    let ownCredential = pcon $ PStakingHash $ pdcons @"_0" # (pdata $ pcon $ PScriptCredential $ pdcons @"_0" # (pdata . pcon . PScriptHash) csByteString # pdnil) # pdnil
+                     in pmatch (AssocMap.plookup # ownCredential # txInfoF.wdrl) $ \case
                             PJust _ -> (popaque $ pconstant ())
                             PNothing -> perror
-                PRewarding stakeCred' ->
-                    let red :: Term _ PTrieAction
-                        red = punsafeCoerce redeemer
-                     in ptrieHandler stakeCred' red ctxF.txInfo
-
--- | TODO (@proxy)
--- validator = multivalidator spend main
+                PRewarding ((pfield @"_0" #) -> stakeCred) ->
+                    let red = punsafeCoerce @_ @_ @PTrieAction redeemer
+                     in pif
+                            (ptrieHandler # stakeCred # red # ctxF.txInfo)
+                            (popaque $ pconstant ())
+                            perror
+                _ -> perror
