c38b6ccd670d4167b4141810dcf7408ea2336604
hade <liveithade@gmail.com>
2024-04-02 15:54:09 +0700

    test: test in progress

--- src/Plutarch/Multivalidator.hs
+++ src/Plutarch/Multivalidator.hs
@@ -1,8 +1,8 @@
 module Plutarch.Multivalidator (validator, multivalidator, spend, main) where
 
-import Plutarch.Api.V1 (PCredential (..))
+-- import Plutarch.Api.V1 (PCredential (..))
 import Plutarch.Api.V1.AssocMap qualified as AssocMap
-import Plutarch.Api.V2 (PScriptContext, PScriptHash (..), PScriptPurpose (..), PStakeValidator, PStakingCredential (..), PValidator)
+import Plutarch.Api.V2 (PScriptContext, PScriptPurpose (..), PStakeValidator, PStakingCredential (..), PValidator)
 import Plutarch.Builtin (pasConstr)
 import Plutarch.Prelude
 import Plutarch.Unsafe (punsafeCoerce)
@@ -57,22 +57,29 @@ main = phoistAcyclic $
     plam $ \redeemer ctx -> unTermCont $ do
         ptraceC "main"
         ctxF <- pletFieldsC @'["txInfo", "purpose"] ctx
-        txInfoF <- pletFieldsC @'["wdrl"] ctxF.txInfo
+        _txInfoF <- pletFieldsC @'["wdrl"] ctxF.txInfo
         return $
             pmatch ctxF.purpose $ \case
-                PMinting ((pfield @"_0" #) -> policy) -> do
-                    let csByteString = (pfromData . pto) policy
-                    let ownCredential = pcon $ PStakingHash $ pdcons @"_0" # (pdata $ pcon $ PScriptCredential $ pdcons @"_0" # (pdata . pcon . PScriptHash) csByteString # pdnil) # pdnil
-                     in pmatch (AssocMap.plookup # ownCredential # txInfoF.wdrl) $ \case
-                            PJust _ -> (popaque $ pconstant ())
-                            PNothing -> perror
+                PMinting ((pfield @"_0" #) -> _policy) -> do
+                    (popaque $ pconstant ())
+                    -- let csByteString = (pfromData . pto) policy
+                    -- let ownCredential = pcon $ PStakingHash $ pdcons @"_0" # (pdata $ pcon $ PScriptCredential $ pdcons @"_0" # (pdata . pcon . PScriptHash) csByteString # pdnil) # pdnil
+                    --  in pmatch (AssocMap.plookup # ownCredential # txInfoF.wdrl) $ \case
+                    --         PJust _ -> (popaque $ pconstant ())
+                    --         PNothing -> perror
                 PRewarding ((pfield @"_0" #) -> stakeCred) ->
                     let red = punsafeCoerce @_ @_ @PTrieAction redeemer
-                     in pif
+                     in 
+                        pif
                             (ptrieHandler # stakeCred # red # ctxF.txInfo)
                             (popaque $ pconstant ())
                             perror
                 _ -> perror
+
+-- main1 :: Term s PStakeValidator
+-- main1 = phoistAcyclic $
+--     plam $ \_redeemer _ctx -> unTermCont $ do
+--       return (popaque $ pconstant ())
 
 validator :: Term s PValidator
 validator = multivalidator main spend
